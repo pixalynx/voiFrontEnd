11353ec82358253a7513d46c451540aa2c8d1987
INFOTRACK\richard.morwood <richard.morwood@infotrack.com.au>
2018-05-16 12:29:51 +1000

    added JS

---
+++ html/src/js/api.js
@@ -0,0 +1,28 @@
+function include(filename)
+{
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.src = filename;
+    script.type = 'text/javascript';
+    head.appendChild(script)
+}
+
+include('http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+var endpoint = ""; /* place our end point here */
+var endpoint = "https://httpbin.org/get";
+     var rest = $.getJSON(endpoint, function(){
+         console.log("success");
+     }).done(function(data){
+         console.log("second success, YAY from api.js");
+         console.log(data);
+     })
+     .fail(function(){
+         console.log("error");
+     })
+     .always(function(){
+         console.log("complete");
+     })
+
+     rest.complete(function(){
+         console.log("second complete");
+     })
