43f9916cd8840170287497a499435e76234b6671
pixalynx <pixalynx@gmail.com>
2018-05-24 00:17:06 +1000

    some changes

--- html/src/js/voi.js
+++ html/src/js/voi.js
@@ -2,7 +2,8 @@ var filestatus = [false,false];
 
 $(document).ready(function(){
     doStuff();
-    buttonClick();
+    APIStuff();
+    //btn_test();
     
       
 })
@@ -58,13 +59,69 @@ function checkFileStatus(){
 
 function buttonClick(){
     $('.scan-btn').click(function(){
+        if(filestatus[0] & filestatus[1] === true){
+            //alert('we can scan YAY');
+            
+            APIStuff();
+        }
         //$("*").hide();
-        $('body').load('loading.html');  
+        //$('body').load('loading.html');  
         //alert('body was loaded'); 
-        setInterval(loadConfirm,5000);
+        //setInterval(loadConfirm,5000);
+        //btn_test();
     })
 }
 
 function loadConfirm(){
     $('body').load('ConfirmView.html');   
+}
+
+function APIStuff(){
+    /* $(document).on('submit', '#theform', function(e){ */
+        
+        $(document).on('submit', '#theform', function(e){
+            e.preventDefault();
+            console.log('this part worked?');
+            // this part works soo far on IOS
+            //alert('this worked');
+                        
+            var form = document.getElementById("theform");
+            var form_data = new FormData($('#theform')[0]);
+            console.log(form.elements.length);
+            for (i = 0; i < form.elements.length; i++) {
+            if (form.elements[i].type == 'file') {
+              if (form.elements[i].value == '') {
+                form.elements[i].parentNode.removeChild(form.elements[i]);
+              }
+            }
+          }
+          $('body').load('loading.html');  
+            $.ajax({
+                type:'POST',
+                url:'http://13.236.119.50:1337/all',
+                enctype : 'multipart/form-data',
+                processData: false,
+                contentType: false,
+                crossDomain: true,  
+                dataType: "json",    
+                async: true,
+                cache: false,     
+                data : form_data,
+                success: function(response){
+                  loadConfirm();
+                  console.log(response);
+                },
+                error : function (e){
+                  console.log('ERROR : ',e);
+                }
+            });
+          });
+}
+
+function btn_test(){
+    $('.scan-btn').click(function(){
+        
+        alert('hello world');
+
+})
 }
